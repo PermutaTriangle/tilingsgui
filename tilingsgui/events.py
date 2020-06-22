62873beccb906f77137489fa727d347fa0c587fa
JonSteinn <jonsteinn@gmail.com>
2020-06-22 09:58:56 -0400

    events cleaned

--- tilingsgui/events.py
+++ tilingsgui/events.py
@@ -1,4 +1,22 @@
+"""Event related module.
+"""
+
+from typing import Iterable
+
+import pyglet
+
+
 class Observer:
-    def __init__(self, dispatchers):
+    """An observer parent class. It handles creating the link between
+    the observer and all its dispatchers.
+    """
+
+    def __init__(self, dispatchers: Iterable[pyglet.event.EventDispatcher]):
+        """Instansiate observer and setup connection wiht dispatchers.
+
+        Args:
+            dispatchers (Iterable[pyglet.event.EventDispatcher]): All event dispatchers
+            that this observer should listen to.
+        """
         for dispatcher in dispatchers:
             dispatcher.push_handlers(self)
